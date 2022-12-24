1bf8d9f122df45b86d008862efebe7dd32bc3c76
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-24 15:16:07 -0300

    wip

--- Sources/LoggerDependency/Logger.swift
+++ Sources/LoggerDependency/Logger.swift
@@ -1,20 +1,26 @@
 import Dependencies
-import Foundation
-@preconcurrency import OSLog
 
-@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
-extension DependencyValues {
-  public var logger: Logger {
-    get { self[Logger.self] }
-    set { self[Logger.self] = newValue }
-  }
-}
+#if canImport(OSLog)
+  // Because of safety reasons, the compiler prevents pass non-literal `OSLogMessage`s to a `Logger`
+  // value. This prevents to create a `Sendable` wrapper. This value is however very likely
+  // thread-safe (to be confirmed), so we can probably use `@preconcurrency` without problem for
+  // now.
+  @preconcurrency import OSLog
 
-@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
-extension Logger: DependencyKey {
-  public static var liveValue: Logger { Logger() }
-  public static var testValue: Logger {
-    XCTFail(#"Unimplemented: @Dependency(\.logger)"#)
-    return Logger()
+  @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
+  extension DependencyValues {
+    public var logger: Logger {
+      get { self[Logger.self] }
+      set { self[Logger.self] = newValue }
+    }
   }
-}
+
+  @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
+  extension Logger: DependencyKey {
+    public static var liveValue: Logger { Logger() }
+    public static var testValue: Logger {
+      XCTFail(#"Unimplemented: @Dependency(\.logger)"#)
+      return Logger()
+    }
+  }
+#endif
