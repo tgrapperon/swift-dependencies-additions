be1f5ea3b868cfa60f4321c14cc27ee0aa761e43
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-24 15:45:06 -0300

    wip

--- Tests/BundleInfoTests/BundleInfoTests.swift
+++ Tests/BundleInfoTests/BundleInfoTests.swift
@@ -1,6 +1,35 @@
-import XCTest
 import BundleInfo
+import Dependencies
+import XCTest
 
 final class BundleInfoTests: XCTestCase {
+  @Dependency(\.bundleInfo) var bundleInfo
 
+  func testBundleInfo() {
+    DependencyValues.withValues {
+      $0.bundleInfo = .init(
+        bundleIdentifier: "com.company.app",
+        name: "Name",
+        displayName: "DisplayName",
+        spokenName: "SpokenName",
+        shortVersion: "1.7",
+        version: "12345"
+      )
+    } operation: {
+      XCTAssertEqual(bundleInfo.bundleIdentifier, "com.company.app")
+      XCTAssertEqual(bundleInfo.name, "Name")
+      XCTAssertEqual(bundleInfo.displayName, "DisplayName")
+      XCTAssertEqual(bundleInfo.spokenName, "SpokenName")
+      XCTAssertEqual(bundleInfo.shortVersion, "1.7")
+      XCTAssertEqual(bundleInfo.version, "12345")
+    }
+  }
+
+  func testFailingTestBundleInfo() {
+    XCTExpectFailure {
+      DependencyValues.withTestValues {
+        XCTAssertEqual(bundleInfo.bundleIdentifier, "")
+      }
+    }
+  }
 }
