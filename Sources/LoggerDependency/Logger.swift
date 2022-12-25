4072a1649483c24716b110bb6fb5f12a75d1239e
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-25 02:17:53 -0300

    wip

--- Sources/LoggerDependency/Logger.swift
+++ Sources/LoggerDependency/Logger.swift
@@ -5,10 +5,12 @@
   // `Logger` value. This prevents to create a `Sendable` wrapper. This value is however very likely
   // thread-safe (to be confirmed), so we can probably use `@preconcurrency` without problem for
   // now.
+  // https://forums.swift.org/t/argument-must-be-a-static-method-or-property-of-oslogprivacy/38441/2
   @preconcurrency import OSLog
 
   @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
   extension DependencyValues {
+    /// A value for writing interpolated string messages to the unified logging system.
     public var logger: Logger {
       get { self[Logger.self] }
       set { self[Logger.self] = newValue }
@@ -26,6 +28,27 @@
 
   @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
   extension Logger {
+    /// Creates a logger using the specified subsystem and category.
+    ///
+    /// You can use this subscript on the `\.logger` dependency:
+    /// ```swift
+    /// @Dependency(\.logger[subsystem: "Backend", category: "Transactions"]) var logger
+    ///
+    /// logger.log("Paid with bank account \(accountNumber)")
+    /// ```
+    public subscript(subsystem subsystem: String, category category: String) -> Logger {
+      return Logger(subsystem: subsystem, category: category)
+    }
+    /// Creates a `Logger` value where messages are categorized by the provided argument.
+    /// The `Logger`'s subsystem is the bundle identifier, extracted from the ``BundleInfo``
+    /// dependency.
+    ///
+    /// You can use this subscript on the `\.logger` dependency:
+    /// ```swift
+    /// @Dependency(\.logger["Transactions"]) var logger
+    ///
+    /// logger.log("Paid with bank account \(accountNumber)")
+    /// ```
     public subscript(category: String) -> Logger {
       @Dependency(\.bundleInfo) var bundleInfo
       return Logger(subsystem: bundleInfo.bundleIdentifier, category: category)
