dcb1544332a8f9ea0404bc41382128fa97b4aff3
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-24 22:08:45 -0300

    wip

--- Tests/LoggerDependencyTests/LoggerDependencyTests.swift
+++ Tests/LoggerDependencyTests/LoggerDependencyTests.swift
@@ -1,5 +1,14 @@
 import XCTest
+import LoggerDependency
+import Dependencies
 
 final class LoggerDependencyTests: XCTestCase {
-
+  @Dependency(\.logger) var logger
+  func testFailingTestLogger() {
+    XCTExpectFailure {
+      DependencyValues.withTestValues {
+        logger.log("TestValue")
+      }
+    }
+  }
 }
