2ec129a6b82c89a2de60afd8eb50aead24788b16
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-29 02:47:26 -0300

    wip

---
+++ Sources/CompressionDependency/Common.swift
@@ -0,0 +1,66 @@
+import Compression
+import Dependencies
+import Foundation
+
+func defaultSync(_ operation: FilterOperation) -> @Sendable (
+  _ data: Data, _ algorithm: Algorithm
+) throws
+  -> Data
+{
+  let operation = UncheckedSendable(operation)
+  return { data, algorithm in
+    let pageSize = 512
+
+    var processed = Data()
+    var index = 0
+    let count = data.count
+    let inputFilter = try InputFilter(
+      operation.wrappedValue,
+      using: algorithm,
+      bufferCapacity: max(65635, pageSize)
+    ) {
+      let rangeLength = min($0, count - index)
+      let subdata = data[index..<index + rangeLength]
+      index += rangeLength
+      return subdata
+    }
+    while let page = try inputFilter.readData(ofLength: pageSize) {
+      processed.append(page)
+    }
+    return processed
+  }
+}
+
+func defaultAsync(_ operation: FilterOperation) -> @Sendable (
+  _ data: Data, _ algorithm: Algorithm
+) async throws -> Data {
+  let operation = UncheckedSendable(operation)
+  return { data, algorithm in
+    let pageSize = 512
+    let asyncOperationsPeriod = 10
+
+    var processed = Data()
+    var index = 0
+    let count = data.count
+    let inputFilter = try InputFilter(
+      operation.wrappedValue,
+      using: algorithm,
+      bufferCapacity: max(65635, pageSize)
+    ) {
+      let rangeLength = min($0, count - index)
+      let subdata = data[index..<index + rangeLength]
+      index += rangeLength
+      return subdata
+    }
+    var iteration: Int = 0
+    while let page = try inputFilter.readData(ofLength: pageSize) {
+      if iteration.isMultiple(of: asyncOperationsPeriod) {
+        try Task.checkCancellation()
+        await Task.yield()
+      }
+      processed.append(page)
+      iteration += 1
+    }
+    return processed
+  }
+}
---
+++ Sources/CompressionDependency/Compressor.swift
@@ -0,0 +1,60 @@
+import Compression
+import Dependencies
+import Foundation
+
+extension DependencyValues {
+  /// A ``Compressor`` that can compress a `Data` value that you supply.
+  public var compress: Compressor {
+    get { self[Compressor.self] }
+    set { self[Compressor.self] = newValue }
+  }
+}
+
+extension Compressor: DependencyKey {
+  public static var liveValue: Self {
+    .default
+  }
+
+  public static var testValue: Self {
+    XCTFail(#"Unimplemented: @Dependency(\.compress)"#)
+    return .default
+  }
+
+  public static var `default`: Compressor {
+    Compressor(defaultSync(.compress), async: defaultAsync(.compress))
+  }
+}
+
+/// A type that compresses `Data` values.
+public struct Compressor: Sendable {
+  private let compress: @Sendable (Data, Algorithm) throws -> Data
+  private let compressAsync: @Sendable (Data, Algorithm) async throws -> Data
+
+  /// Creates a ``Compressor`` value by providing a closure that compresses the provided
+  /// data and algorithm.
+  ///
+  /// You can optionally specify an async version that may optimize the operation in async
+  /// contexts. If no variant is specified, the sync version is used for sync and async calls.
+  public init(
+    _ compress: @escaping @Sendable (Data, Algorithm) throws -> Data,
+    async compressAsync: (@Sendable (Data, Algorithm) async throws -> Data)? = nil
+  ) {
+    self.compress = { @Sendable in try compress($0, $1) }
+    self.compressAsync = { @Sendable in try await compressAsync?($0, $1) ?? compress($0, $1) }
+  }
+  
+  /// Compresses the provided data synchronously, using the provided algorithm.
+  public func callAsFunction(
+    _ data: Data,
+    using algorithm: Algorithm = .zlib
+  ) throws -> Data {
+    try self.compress(data, algorithm)
+  }
+  /// Compresses the provided data asynchronously, using the provided algorithm.
+  public func callAsFunction(
+    _ data: Data,
+    using algorithm: Algorithm = .zlib
+  ) async throws -> Data {
+    try await self.compressAsync(data, algorithm)
+  }
+}
