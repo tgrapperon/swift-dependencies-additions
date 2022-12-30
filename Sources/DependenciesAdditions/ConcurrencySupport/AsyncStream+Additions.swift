0a566f1f795c983c89638e474e3aa3311f580601
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-30 08:59:35 -0300

    wip

--- Sources/DependenciesAdditions/ConcurrencySupport/AsyncStream+Additions.swift
+++ Sources/DependenciesAdditions/ConcurrencySupport/AsyncStream+Additions.swift
@@ -1,5 +1,9 @@
-
 extension AsyncStream {
+  /// Produces an `AsyncStream` from an async `AsyncSequence` by awaiting and then consuming the
+  /// sequence till it terminates, ignoring any failure.
+  ///
+  /// Useful as a kind of type eraser for actor-isolated live `AsyncSequence`-based dependencies, that
+  /// also erases the `async` extraction.
   public init<S: AsyncSequence>(_ sequence: @escaping () async throws -> S) throws
   where S.Element == Element {
     var iterator: S.AsyncIterator?
@@ -8,19 +12,6 @@ extension AsyncStream {
         iterator = try? await sequence().makeAsyncIterator()
       }
       return try? await iterator?.next()
-    }
-  }
-}
-
-extension AsyncStream  {
-  public init<S: Sequence>(_ sequence: @escaping () async throws -> S)
-  where S.Element == Element {
-    var iterator: S.Iterator?
-    self.init {
-      if iterator == nil {
-        iterator = try? await sequence().makeIterator()
-      }
-      return iterator?.next()
     }
   }
 }
