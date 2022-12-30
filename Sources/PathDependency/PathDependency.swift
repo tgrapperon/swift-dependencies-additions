6778c2f32a7301dd626beac74c182d1cf309b920
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-29 23:56:55 -0300

    wip

--- Sources/PathDependency/PathDependency.swift
+++ Sources/PathDependency/PathDependency.swift
@@ -18,8 +18,10 @@ public struct Path: Hashable, @unchecked Sendable {
   let lock = NSRecursiveLock()
   private var _components = [AnyHashable]()
 
+  /// Creates an empty path.
   public static var empty: Path { .init() }
 
+  /// The components of the path, under the form of an array of `AnyHashable`.
   public var components: [AnyHashable] {
     // components are guaranteed to be `Sendable` by the exposed API.
     lock.lock()
@@ -27,18 +29,21 @@ public struct Path: Hashable, @unchecked Sendable {
     return _components
   }
 
+  /// Appends a new component to a path.
   public mutating func push<Component: Hashable & Sendable>(_ component: Component) {
     lock.lock()
     defer { lock.unlock() }
     _components.append(component)
   }
 
+  /// Returns a new ``Path`` with provided component being appended to it.
   public func pushing<Component: Hashable & Sendable>(_ component: Component) -> Self {
     var path = self
     path.push(component)
     return path
   }
 
+  /// Removes the last component if any. Does nothing if the path is empty.
   public mutating func popLast() {
     lock.lock()
     defer { lock.unlock() }
@@ -47,7 +52,9 @@ public struct Path: Hashable, @unchecked Sendable {
     }
   }
 
+  /// Returns a new ``Path`` where the last component has been removed.
   public func poppingLast() -> Self {
+    if _components.isEmpty { return self }
     var path = self
     path.popLast()
     return path
