d498850e001e6952fa68df633867b6df6226ad39
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2022-12-30 09:10:54 -0300

    wip

--- Sources/DependenciesAdditions/LazyProxy.swift
+++ Sources/DependenciesAdditions/LazyProxy.swift
@@ -1,4 +1,9 @@
 import Foundation
+/// A property wrapper that delegates the storage of a property to an internal closure.
+///
+/// This property wrapper is useful when defining dependencies as abstractions of existing types
+/// exposing properties, as the live implementation can lazily delegate calls to some abstracted
+/// type's instance.
 @propertyWrapper
 public struct LazyProxy<Value> {
   private let lock = NSRecursiveLock()
@@ -22,6 +27,13 @@ public struct LazyProxy<Value> {
 
 extension LazyProxy: @unchecked Sendable where Value: Sendable {}
 
+/// A property wrapper that delegates the storage of a property to a `@MainActor` internal closure.
+///
+/// This property wrapper is useful when defining dependencies as abstractions of existing
+/// `@MainActor`-isolated types that are exposing properties, as the live implementation can lazily
+/// delegate calls to some instance of the abstracted type's. In such cases, it also facilitate the
+/// creation of these abtracted dependencies' values in non-isolated context, with is a requirement
+/// of `DependencyKey`.
 @propertyWrapper
 public struct MainActorLazyProxy<Value> {
   private var value: @MainActor () -> Value
