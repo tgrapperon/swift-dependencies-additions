2e93b7febc31271c86a9ffc347e2d6db50387dda
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-01-09 19:34:40 -0300

    wip

--- Tests/_AppStorageDependencyTests/AppStorageTests.swift
+++ Tests/_AppStorageDependencyTests/AppStorageTests.swift
@@ -47,14 +47,96 @@ final class AppStorageTests: XCTestCase {
     }
   }
   #if os(macOS)
-  func testStream() async throws {
-    final class Model: @unchecked Sendable {
-      @Dependency.AppStorage("SomeKey") var int = 42
+    func testStream() async throws {
+      final class Model: @unchecked Sendable {
+        @Dependency.AppStorage("SomeKey") var int = 42
+      }
+      let model = Model()
+      await withDependencies {
+        $0.userDefaults = .ephemeral()
+      } operation: {
+        await withTimeout(2000) { group in
+          group.addTask {
+            let expectations: [Int] = [42, 55, 42, 20, 446, 42]
+            var index = 0
+            for await element in model.$int {
+              XCTAssertEqual(element, expectations[index])
+              index += 1
+              if index == expectations.endIndex {
+                return
+              }
+            }
+          }
+          group.addTask {
+            /// Let the first value hit the enumeration
+            try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
+            model.int = 55
+            try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
+            model.int = 42
+            try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
+            model.int = 20
+            try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
+            model.int = 446
+            try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
+            model.$int.reset()
+          }
+        }
+      }
     }
-    let model = Model()
-    await withDependencies {
-      $0.userDefaults = .ephemeral()
-    } operation: {
+
+    func testLiveURLAppStorage() {
+      let url1 = URL(string: "https://pointfree.co/")!
+      let url2 = URL(string: "https://www.google.com/")!
+      let fileURL1 = FileManager.default.temporaryDirectory.appendingPathComponent("1")
+      let fileURL2 = FileManager.default.temporaryDirectory.appendingPathComponent("2")
+
+      UserDefaults.standard.removeObject(forKey: "URL")
+      UserDefaults.standard.removeObject(forKey: "FileURL")
+      defer {
+        UserDefaults.standard.removeObject(forKey: "URL")
+        UserDefaults.standard.removeObject(forKey: "FileURL")
+      }
+
+      @Dependency.AppStorage("URL") var url = url1
+      @Dependency.AppStorage("FileURL") var fileURL: URL = fileURL1
+
+      @Dependency.AppStorage("URL") var sameURL: URL?
+      @Dependency.AppStorage("FileURL") var sameFileURL: URL?
+
+      let _ = __dummySeparator__
+
+      withDependencies {
+        $0.context = .live
+      } operation: {
+        XCTAssertEqual(url, url1)
+        XCTAssertEqual(fileURL, fileURL1)
+
+        url = url2
+        XCTAssertEqual(url, url2)
+        XCTAssertEqual(url, sameURL)
+
+        fileURL = fileURL2
+        XCTAssertEqual(fileURL, fileURL2)
+        XCTAssertEqual(fileURL, sameFileURL)
+      }
+    }
+
+    func testLiveStream() async throws {
+
+      final class Model: @unchecked Sendable {
+        @Dependency.AppStorage("SomeKey") var int = 42
+      }
+
+      let model = withDependencies {
+        $0.userDefaults = .standard
+      } operation: {
+        Model()
+      }
+
+      UserDefaults.standard.removeObject(forKey: "SomeKey")
+      defer {
+        UserDefaults.standard.removeObject(forKey: "SomeKey")
+      }
       await withTimeout(2000) { group in
         group.addTask {
           let expectations: [Int] = [42, 55, 42, 20, 446, 42]
@@ -82,89 +164,5 @@ final class AppStorageTests: XCTestCase {
         }
       }
     }
-  }
-  #endif
-
-  func testLiveURLAppStorage() {
-    let url1 = URL(string: "https://pointfree.co/")!
-    let url2 = URL(string: "https://www.google.com/")!
-    let fileURL1 = FileManager.default.temporaryDirectory.appendingPathComponent("1")
-    let fileURL2 = FileManager.default.temporaryDirectory.appendingPathComponent("2")
-
-    UserDefaults.standard.removeObject(forKey: "URL")
-    UserDefaults.standard.removeObject(forKey: "FileURL")
-    defer {
-      UserDefaults.standard.removeObject(forKey: "URL")
-      UserDefaults.standard.removeObject(forKey: "FileURL")
-    }
-
-    @Dependency.AppStorage("URL") var url = url1
-    @Dependency.AppStorage("FileURL") var fileURL: URL = fileURL1
-
-    @Dependency.AppStorage("URL") var sameURL: URL?
-    @Dependency.AppStorage("FileURL") var sameFileURL: URL?
-
-    let _ = __dummySeparator__
-
-    withDependencies {
-      $0.context = .live
-    } operation: {
-      XCTAssertEqual(url, url1)
-      XCTAssertEqual(fileURL, fileURL1)
-
-      url = url2
-      XCTAssertEqual(url, url2)
-      XCTAssertEqual(url, sameURL)
-
-      fileURL = fileURL2
-      XCTAssertEqual(fileURL, fileURL2)
-      XCTAssertEqual(fileURL, sameFileURL)
-    }
-  }
-
-  #if os(macOS)
-  func testLiveStream() async throws {
-
-    final class Model: @unchecked Sendable {
-      @Dependency.AppStorage("SomeKey") var int = 42
-    }
-
-    let model = withDependencies {
-      $0.userDefaults = .standard
-    } operation: {
-      Model()
-    }
-
-    UserDefaults.standard.removeObject(forKey: "SomeKey")
-    defer {
-      UserDefaults.standard.removeObject(forKey: "SomeKey")
-    }
-    await withTimeout(2000) { group in
-      group.addTask {
-        let expectations: [Int] = [42, 55, 42, 20, 446, 42]
-        var index = 0
-        for await element in model.$int {
-          XCTAssertEqual(element, expectations[index])
-          index += 1
-          if index == expectations.endIndex {
-            return
-          }
-        }
-      }
-      group.addTask {
-        /// Let the first value hit the enumeration
-        try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
-        model.int = 55
-        try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
-        model.int = 42
-        try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
-        model.int = 20
-        try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
-        model.int = 446
-        try await Task.sleep(nanoseconds: 200 * NSEC_PER_MSEC)
-        model.$int.reset()
-      }
-    }
-  }
   #endif
 }
