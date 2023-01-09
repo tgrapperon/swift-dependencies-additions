866121584dccc84042af5acedf87e8f9a92f9ab9
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-01-09 15:07:58 -0300

    wip

--- Sources/DependenciesAdditionsBasics/TestSupport/WithTimeout.swift
+++ Sources/DependenciesAdditionsBasics/TestSupport/WithTimeout.swift
@@ -1,7 +1,7 @@
 import Foundation
 import XCTestDynamicOverlay
 #if os(Linux)
-let NSEC_PER_MSEC: UInt64 = 1_000_000
+public let NSEC_PER_MSEC: UInt64 = 1_000_000
 #endif
 
 /// Performs an async operation that fails if it hasn't finished before a timeout expires.
