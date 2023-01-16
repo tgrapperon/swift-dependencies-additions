02dacbe25ac67badd6a16bc1293ddbf8cf36bdaa
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-01-15 23:49:30 -0300

    Improve the `Logger` study with an embedded console (#22)

--- Examples/CaseStudies/CaseStudies/Studies/LoggerStudy.swift
+++ Examples/CaseStudies/CaseStudies/Studies/LoggerStudy.swift
@@ -1,4 +1,5 @@
 import DependenciesAdditions
+import OSLog
 import SwiftUI
 
 final class LoggerStudy: ObservableObject {
@@ -28,7 +29,7 @@ final class LoggerStudy: ObservableObject {
       do {
         try await purchase(instrument: instrument)
         transactionsLogger.info(
-          "\(customerName) did succesfully purchase a \(instrument.rawValue, privacy: .public)"
+          "\(customerName, privacy: .private) did succesfully purchase a \(instrument.rawValue, privacy: .public)"
         )
       } catch {
         transactionsLogger.error(
@@ -37,11 +38,11 @@ final class LoggerStudy: ObservableObject {
       }
     }
   }
-
+  /// Purchases fail 30% of the time.
   func purchase(instrument: Instrument) async throws {
     try await Task.sleep(for: .milliseconds(250))
     let randomNumber = withRandomNumberGenerator {
-      Int.random(in: 0 ... 2, using: &$0)
+      Int.random(in: 0...2, using: &$0)
     }
     if randomNumber == 1 {
       throw PurchaseError.badLuck
@@ -51,29 +52,178 @@ final class LoggerStudy: ObservableObject {
 
 struct LoggerStudyView: View {
   @ObservedObject var model: LoggerStudy
+  @StateObject.Dependency var loggerModel = LoggerModel()
   var body: some View {
     VStack {
-      HStack {
-        Button {
-          model.purchaseInstrumentButtonTapped(instrument: .guitar)
-        } label: {
-          Label("Buy a guitar", systemImage: "guitars")
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 9)
+      VStack(alignment: .leading) {
+        HStack {
+          Button {
+            model.purchaseInstrumentButtonTapped(instrument: .guitar)
+          } label: {
+            Label("Buy a guitar", systemImage: "guitars")
+              .frame(maxWidth: .infinity)
+              .padding(.vertical, 9)
+          }
+          Button {
+            model.purchaseInstrumentButtonTapped(instrument: .piano)
+          } label: {
+            Label("Buy a piano", systemImage: "pianokeys")
+              .frame(maxWidth: .infinity)
+              .padding(.vertical, 9)
+          }
         }
-        Button {
-          model.purchaseInstrumentButtonTapped(instrument: .piano)
-        } label: {
-          Label("Buy a piano", systemImage: "pianokeys")
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 9)
-        }
+        .buttonStyle(.borderedProminent)
+        Text("Check the console while you tap the buttons above…")
+        Text(
+          "On a real device that is not connected to a debugger, the private fields of log entries will be redacted."
+        )
+        .font(.footnote)
+        .foregroundStyle(.secondary)
       }
-      .buttonStyle(.borderedProminent)
-      Text("Check the console while you tap the buttons above…")
+      .padding()
+      ConsoleView(loggerModel: loggerModel)
     }
-    .padding()
     .navigationTitle("Logger")
+  }
+}
+struct ConsoleView: View {
+  @ObservedObject var loggerModel: LoggerModel
+  var body: some View {
+    List {
+      ForEach(loggerModel.messages.reversed(), id: \.self) { message in
+        VStack(alignment: .leading) {
+          HStack(alignment: .firstTextBaseline) {
+            Text(message.date.formatted(.dateTime.day().month().year().hour().minute().second()))
+            Spacer()
+            Text(message.level.localizedDescription)
+          }
+          .font(.callout)
+          if !message.subsystem.isEmpty {
+            Text("\(message.subsystem)")
+              .font(.headline)
+              .frame(maxWidth: .infinity, alignment: .leading)
+          }
+          if !message.category.isEmpty {
+            Text("\(message.category)")
+              .font(.subheadline)
+              .frame(maxWidth: .infinity, alignment: .leading)
+          }
+          Text(message.composedMessage)
+            .lineLimit(2)
+            .font(.callout)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .foregroundStyle(message.level.color)
+        .scaleEffect(x: 1, y: -1)
+      }
+      .listRowInsets(.init(top: 4, leading: 4, bottom: 4, trailing: 2))
+    }
+    .overlay {
+      if self.loggerModel.isLoading {
+        ProgressView()
+          .progressViewStyle(.automatic)
+      }
+    }
+    .scaleEffect(x: 1, y: -1)
+    .listStyle(.plain)
+  }
+}
+
+@MainActor
+final class LoggerModel: ObservableObject {
+  @Published var messages: [OSLogEntryLog] = []
+  @Published var isLoading: Bool = false
+  @Dependency(\.continuousClock) var clock
+  @Dependency(\.bundleInfo.bundleIdentifier) var bundleIdentifier
+  var cancellables: Set<AnyCancellableTask> = []
+
+  init() {
+    Task.detached { [weak self] in
+      guard let self else { return }
+      do {
+        await self.setIsLoading(true)
+        try await self.retrieveNewEntries(
+          current: self.messages,
+          subsystem: self.bundleIdentifier
+        )
+        await self.setIsLoading(false)
+        for await _ in await self.clock.timer(interval: .seconds(1)) {
+          try await self.retrieveNewEntries(
+            current: self.messages,
+            subsystem: self.bundleIdentifier
+          )
+        }
+      } catch {
+        print(error)
+      }
+    }.store(in: &cancellables)
+  }
+
+  func setIsLoading(_ isLoading: Bool) {
+    self.isLoading = isLoading
+  }
+  func updateMessages(_ messages: [OSLogEntryLog]) {
+    self.messages = messages
+  }
+
+  nonisolated
+    func retrieveNewEntries(
+      current: [OSLogEntryLog],
+      subsystem: String
+    ) async throws
+  {
+    let store = try OSLogStore(scope: .currentProcessIdentifier)
+    let date = await MainActor.run { current.last?.date }
+    let newEntries = try store.getEntries(
+      with: [],
+      at: store.position(date: .now.addingTimeInterval(-1.5)),
+      matching: nil  // Can't make it work on `subsystem` for some reason…
+    )
+    .lazy
+    .compactMap { $0 as? OSLogEntryLog }
+    .filter { $0.date > (date ?? .distantPast) }
+    .filter { $0.subsystem.isEmpty || $0.subsystem == subsystem }
+    await self.updateMessages(current + newEntries)
+  }
+}
+
+extension OSLogEntryLog.Level {
+  var localizedDescription: String {
+    switch self {
+    case .undefined:
+      return "Undefined"
+    case .debug:
+      return "Debug"
+    case .info:
+      return "Info"
+    case .notice:
+      return "Notice"
+    case .error:
+      return "Error"
+    case .fault:
+      return "Fault"
+    @unknown default:
+      return "?"
+    }
+  }
+
+  var color: Color {
+    switch self {
+    case .undefined:
+      return .primary
+    case .debug:
+      return .purple
+    case .info:
+      return .blue
+    case .notice:
+      return .indigo
+    case .error:
+      return .red
+    case .fault:
+      return .pink
+    @unknown default:
+      return .primary
+    }
   }
 }
 
