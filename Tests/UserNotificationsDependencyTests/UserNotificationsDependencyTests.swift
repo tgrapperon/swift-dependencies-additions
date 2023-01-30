e935d05cb813984d8826135c5b42314c73d6110c
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-01-30 17:37:16 -0300

    wip

--- Tests/UserNotificationsDependencyTests/UserNotificationsDependencyTests.swift
+++ Tests/UserNotificationsDependencyTests/UserNotificationsDependencyTests.swift
@@ -1,50 +1,50 @@
+import Dependencies
 import Foundation
 import UserNotificationsDependency
-import Dependencies
 import XCTest
 
 final class UserNotificationsDependencyTests: XCTestCase {
   @Dependency(\.userNotificationCenter) var notifications
-  
+
   // This one `fatalError`'s, as we can't build a placeholder.
-//  func testUnimplmentedNotificationSettings() async {
-//    XCTExpectFailure {
-//      let _ = await notifications.notificationSettings()
-//    }
-//  }
-  
+  //  func testUnimplmentedNotificationSettings() async {
+  //    XCTExpectFailure {
+  //      let _ = await notifications.notificationSettings()
+  //    }
+  //  }
+
   func testUnimplementedDelegate() {
     XCTExpectFailure {
       let _ = notifications.delegate
     }
   }
-  
+
   func testUnimplementedSupportsContentExtensions() {
     XCTExpectFailure {
       let _ = notifications.supportsContentExtensions
     }
   }
-  
+
   @available(iOS 16.0, macOS 13, *)
   func testUnimplmentedSetBadgeCount() async throws {
-//    XCTExpectFailure {
-//      let _ = try await notifications.setBadgeCount(0)
-//    }
+    //    XCTExpectFailure {
+    //      let _ = try await notifications.setBadgeCount(0)
+    //    }
   }
   func testUnimplmentedRequestAuthorization() async throws {
-//    XCTExpectFailure {
-//      let _ = try await notifications.requestAuthorization()
-//    }
+    //    XCTExpectFailure {
+    //      let _ = try await notifications.requestAuthorization()
+    //    }
   }
   func testUnimplmentedAdd() async throws {
-//    XCTExpectFailure {
-//      let _ = try await notifications.add(.init(identifier: "", content: .init(), trigger: nil))
-//    }
+    //    XCTExpectFailure {
+    //      let _ = try await notifications.add(.init(identifier: "", content: .init(), trigger: nil))
+    //    }
   }
   func testUnimplmentedPendingNotificationRequests() async {
-//    XCTExpectFailure {
-//      let _ = await notifications.pendingNotificationRequests()
-//    }
+    //    XCTExpectFailure {
+    //      let _ = await notifications.pendingNotificationRequests()
+    //    }
   }
   func testUnimplmentedRemovePendingNotificationRequests() {
     XCTExpectFailure {
@@ -57,9 +57,9 @@ final class UserNotificationsDependencyTests: XCTestCase {
     }
   }
   func testUnimplmentedDeliveredNotifications() async {
-//    XCTExpectFailure {
-//      let _ = await notifications.deliveredNotifications()
-//    }
+    //    XCTExpectFailure {
+    //      let _ = await notifications.deliveredNotifications()
+    //    }
   }
   func testUnimplmentedRemoveDeliveredNotifications() {
     XCTExpectFailure {
@@ -77,19 +77,8 @@ final class UserNotificationsDependencyTests: XCTestCase {
     }
   }
   func testUnimplmentedNotificationCategories() async {
-//    XCTExpectFailure {
-//      let _ = await notifications.notificationCategories()
-//    }
+    //    XCTExpectFailure {
+    //      let _ = await notifications.notificationCategories()
+    //    }
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 }
