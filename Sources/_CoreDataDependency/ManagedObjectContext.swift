b6fce891ea79a4a845be9a0f0b8a8a773d52210c
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-04-20 21:03:57 +0900

    Update ManagedObjectContext.swift

--- Sources/_CoreDataDependency/ManagedObjectContext.swift
+++ Sources/_CoreDataDependency/ManagedObjectContext.swift
@@ -1,34 +1,36 @@
-import CoreData
-import Dependencies
+#if canImport(CoreData)
+  import CoreData
+  import Dependencies
 
-public final class ViewContext: AnyManagedObjectContext {
-  override init(_ managedObjectContext: NSManagedObjectContext) {
-    assert(managedObjectContext.concurrencyType == .mainQueueConcurrencyType)
-    super.init(managedObjectContext)
-  }
-}
-
-public class AnyManagedObjectContext: @unchecked Sendable, Hashable {
-  let _managedObjectContext: UncheckedSendable<NSManagedObjectContext>
-  init(_ managedObjectContext: NSManagedObjectContext) {
-    self._managedObjectContext = .init(wrappedValue: managedObjectContext)
-  }
-
-  public static func == (lhs: AnyManagedObjectContext, rhs: AnyManagedObjectContext) -> Bool {
-    lhs._managedObjectContext == rhs._managedObjectContext
-  }
-  public func hash(into hasher: inout Hasher) {
-    _managedObjectContext.hash(into: &hasher)
-  }
-}
-
-extension AnyManagedObjectContext {
-  public static func managedObjectContext(_ managedObjectContext: NSManagedObjectContext)
-    -> AnyManagedObjectContext
-  {
-    if managedObjectContext.concurrencyType == .mainQueueConcurrencyType {
-      return ViewContext(managedObjectContext)
+  public final class ViewContext: AnyManagedObjectContext {
+    override init(_ managedObjectContext: NSManagedObjectContext) {
+      assert(managedObjectContext.concurrencyType == .mainQueueConcurrencyType)
+      super.init(managedObjectContext)
     }
-    return AnyManagedObjectContext(managedObjectContext)
   }
-}
+
+  public class AnyManagedObjectContext: @unchecked Sendable, Hashable {
+    let _managedObjectContext: UncheckedSendable<NSManagedObjectContext>
+    init(_ managedObjectContext: NSManagedObjectContext) {
+      self._managedObjectContext = .init(wrappedValue: managedObjectContext)
+    }
+
+    public static func == (lhs: AnyManagedObjectContext, rhs: AnyManagedObjectContext) -> Bool {
+      lhs._managedObjectContext == rhs._managedObjectContext
+    }
+    public func hash(into hasher: inout Hasher) {
+      _managedObjectContext.hash(into: &hasher)
+    }
+  }
+
+  extension AnyManagedObjectContext {
+    public static func managedObjectContext(_ managedObjectContext: NSManagedObjectContext)
+      -> AnyManagedObjectContext
+    {
+      if managedObjectContext.concurrencyType == .mainQueueConcurrencyType {
+        return ViewContext(managedObjectContext)
+      }
+      return AnyManagedObjectContext(managedObjectContext)
+    }
+  }
+#endif
