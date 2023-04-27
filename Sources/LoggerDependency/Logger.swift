d63b36f9d2f77445e14761f0dc41f9e1b89376e6
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-04-27 19:31:19 +0700

    Merge upstream (#57)
    Co-authored-by: Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
    Co-authored-by: tgrapperon <tgrapperon@users.noreply.github.com>
    Co-authored-by: atacan <atacandur@icloud.com>
    Fix DocC references (#55)

--- Sources/LoggerDependency/Logger.swift
+++ Sources/LoggerDependency/Logger.swift
@@ -21,11 +21,19 @@
   @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
   extension Logger: DependencyKey {
     public static var liveValue: Logger { Logger() }
-    public static var testValue: Logger {
+    /// - Note: It doesn't make a lot of sense to fail by default because we can't pass an
+    /// inspectable `Logger` value to assert logged messages when testing. Given the prominence of
+    /// logging failing by default is more annoying than useful in real-life scenarios.
+    /// Users who wish to assert that no logging occurs can override the `\.logger` dependency with
+    /// the `.unimplemented` logger.
+    public static var testValue: Logger { Logger() }
+    public static var previewValue: Logger { Logger() }
+
+    /// A `Logger` that fails when accessed while testing.
+    public static var unimplemented: Logger {
       XCTFail(#"Unimplemented: @Dependency(\.logger)"#)
       return Logger()
     }
-    public static var previewValue: Logger { Logger() }
   }
 
   @available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
@@ -52,6 +60,14 @@
     /// ```
     public subscript(category: String) -> Logger {
       Logger(subsystem: Bundle.main.bundleIdentifier ?? "", category: category)
+    }
+  }
+
+  @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+  extension Logger {
+    /// Creates a `OSSignposter` to emit signpost to this logger value.
+    public var signpost: OSSignposter {
+      OSSignposter(logger: self)
     }
   }
 #endif
