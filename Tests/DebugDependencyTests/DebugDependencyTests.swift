a077682a2d8eb8d203f8f2712d61b0fb284e965b
Thomas Grapperon <35562418+tgrapperon@users.noreply.github.com>
2023-04-27 19:33:29 +0700

    wip

--- Tests/DebugDependencyTests/DebugDependencyTests.swift
+++ Tests/DebugDependencyTests/DebugDependencyTests.swift
@@ -6,21 +6,6 @@ import XCTest
 final class DebugDependencyTests: XCTestCase {
   @Dependency(\.debug) var debug
   func testStack() {
-    withDependencies {
-      $0.logger = .liveValue
-    } operation: {
-      self.debug.stackDepth()
-      self.debug.stackDepth()
-      self.debug.stackDepth(label: "testt")
-      let x = 1000
-      self.debug.stackDepth()
-      self.debug.stackDepth()
-    }
-
-    
-//      withDependencies { $0.logger = .liveValue } operation: {
-//        @Dependency(\.logger["Logger.Dependency.Testing"].stackDepth) var stackDepth
-//        stackDepth()
-//      }
+    self.debug.stackDepth()
   }
 }
